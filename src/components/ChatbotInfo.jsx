871822e792aa2b4aeafd167158b40732540e05bf
walthersc11 <gamersc11@hotmail.com>
2024-10-21 11:44:09 -0500

    Se agrego condicional al boton de registro del inicio

--- src/components/ChatbotInfo.jsx
+++ src/components/ChatbotInfo.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+"use client";
+import React,{useEffect,useState} from 'react'
 import Image from 'next/image'
+import axios from 'axios';
 
 export default function ChatbotInfo () {
+  const [mostrarBoton, setMostrarBoton] = useState(true);
   const imagenes = {
     rightarrow: <Image src="/right-arrow.svg" width={26} height={16}/>
   }
+  useEffect(()=>{
+    const getToken = async ()=>{
+      try{
+      const data = await axios.get('/api/getToken')
+      const correo = data.data.email
+      if (correo){setMostrarBoton(false)}
+    }catch(e){
+    }
+  }
+    getToken();
+  },[]);
+ 
   return (
     <section className='flex md:flex-row flex-col sm:py-16 p-16 '>
       <div className='flex-1 flex justify-center justify-items-start flex-col px-16 sm:px-16'>
@@ -25,10 +40,12 @@ export default function ChatbotInfo () {
         <p className='font-poppins font-normal text-white text-[18px] leading-[30.8px] max-w-[470px] mt-5'>
           Habla con Mapri-Chatbot y recibe un diagnostico inmediato sobre tu estado de salud mental a cualquier hora del día.
         </p>
-        <button className='text-[25px] shadow-lg font-bold h-14 w-[200px] text-slate-50 my-6 rounded-full 
+        {mostrarBoton &&(
+        <button id='btnRegistrar' className='text-[25px] shadow-lg font-bold h-14 w-[200px] text-slate-50 my-6 rounded-full 
         bg-gradient-to-r from-purple-500 to-teal-400 hover:from-sky-700 hover:to-cyan-400'> 
          <a href='/Registrar'>Regístrate</a>
         </button>
+        )}
       </div>
 
       <div className='flex-1 flex justify-center items-center md:my-0 my-10 relative'>
